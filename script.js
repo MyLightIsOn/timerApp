13b7180467474d12c82c8f3a1b437fed78e2b0de
Lawrence Moore <lawrence2978@hotmail.com>
2016-02-13 09:12:03 -0500

    Added some DOM elements and event handlers

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-/**
- * User: Lawrence
- * Date: 2/13/2016
- * Time: 8:33 AM
- */
+var timerApp = timerApp || {};
+
+timerApp = {
+	init: function(){
+		var	minutes = document.getElementById('minutes'),
+			seconds = document.getElementById('seconds'),
+			milliseconds = document.getElementById('milliseconds'),
+			start = document.getElementsByClassName('start'),
+			stop = document.getElementsByClassName('stop');
+
+		timerApp.addHandlers(start, stop)
+	},
+
+	addHandlers: function(start, stop){
+		start[0].addEventListener('click', function(){
+			timerApp.startTimer()
+		});
+
+		stop[0].addEventListener('click', function(){
+			timerApp.stopTimer()
+		});
+	},
+
+	startTimer: function(){
+		console.log('Timer started')
+	},
+
+	stopTimer: function(){
+		console.log('Timer stopped')
+	}
+};
+
+timerApp.init();
